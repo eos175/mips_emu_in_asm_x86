3d46dba2479ea564019282cc56ce7777409313d0
= <=>
2020-09-10 18:43:43 -0600

    Se agregan los opcodes y funct

--- mips.asm
+++ mips.asm
@@ -1,4 +1,4 @@
-
+ 
 
 %define get_register(i ,r)      mov r, [registers + i*4]
 %define set_register(i, r)      mov [registers + i*4], r
@@ -17,6 +17,55 @@ para las operaciones inmediatas, almacenamos en
 
 
 %endif
+
+;-------------------Format R [Funct]-------------------------------------;
+%define	_add_r		0x20
+%define _addu_r		0x21
+%define _and_r		0x24
+%define _div_r		0x1A
+%define _divu_r		0x1B
+%define _jr_r		0x08
+%define	_mfhi_r		0x10
+%define _mthi_r		0x11
+%define _mflo_r		0x12
+%define _mtlo_r		0x13
+%define _mult_r		0x18
+%define _multu_r	0x19
+%define _nor_r		0x27
+%define _xor_r		0x26
+%define _or_r		0x25
+%define _slt_r		0x2A
+%define _sltu_r		0x2B
+%define _sll_r		0x00
+%define _srl_r		0x02
+%define _sra_r		0x03
+%define _sub_r		0x22
+%define _subu_r		0x23
+
+
+;-------------------Format I [Opcode]------------------------------------;
+%define _addi_i		0x08
+%define _addiu_i	0x09
+%define _andi_i		0x0C
+%define _beq_i		0x04
+%define _blez_i		0x06
+%define _bne_i		0x05
+%define _bgtz_i 	0x07
+%define _lb_i		0x20
+%define _lbu_i		0x24
+%define _lhu_i		0x25
+%define _lui_i		0x0F
+%define _lw_i		0x23
+%define _ori_i		0x0D
+%define _sb_i		0x28
+%define _sh_i 		0x29
+%define _slti_i		0x0A
+%define _sltiu_i	0x0B
+%define _sw_i		0x2B
+
+;-------------------Format J [Opcode]------------------------------------;
+%define _j_j		0x02
+%define _jal_j		0x03
 
 
 
